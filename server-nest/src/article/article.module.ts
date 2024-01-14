184cc484fcdc8849a0107e451125aefaefa2f941
Tubagus Saifulloh <dutapanggulu@gmail.com>
2024-01-15 06:07:04 +0700

    fix repository module not imported

--- server-nest/src/article/article.module.ts
+++ server-nest/src/article/article.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common'
 import { ArticleService } from './article.service'
 import { ArticleController } from './article.controller'
 import { AuthUser } from '@pengode/auth/utils/auth-user'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Article } from '@pengode/article/article'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Article])],
   providers: [AuthUser, ArticleService],
   controllers: [ArticleController],
 })
