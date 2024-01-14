6852d0202a968ab27566c3b132330a71fae6d3b2
Tubagus Saifulloh <dutapanggulu@gmail.com>
2024-01-15 06:07:46 +0700

    create custom decorator for binding page request

--- server-nest/src/common/dtos.ts
+++ server-nest/src/common/dtos.ts
@@ -1,3 +1,5 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common'
+
 export class PageResponse<T> {
   items: T[]
   nextCursor: number
@@ -8,3 +10,14 @@ export class PageRequest<T = unknown> {
   cursor: number = Number.MAX_VALUE
   filter?: T
 }
+
+export const PageParam = createParamDecorator(
+  (_data: unknown, ctx: ExecutionContext): PageRequest => {
+    const request = ctx.switchToHttp().getRequest()
+    const query = request.query
+
+    const size = parseInt(query.size) || 10
+    const cursor = parseInt(query.cursor) || Math.pow(2, 31) - 1
+    return { size, cursor }
+  },
+)
