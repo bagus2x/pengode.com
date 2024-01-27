6b84d017a4e113d8349c0ef528aed21f7e33117e
Tubagus Saifulloh <dutapanggulu@gmail.com>
2024-01-28 03:41:04 +0700

    import ProductInvoiceItem module

--- server-nest/src/product/product.module.ts
+++ server-nest/src/product/product.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
+
 import { ProductCategory } from '@pengode/product-category/product-category'
 import { Product } from '@pengode/product/product'
-import { ProductController } from './product.controller'
-import { ProductService } from './product.service'
+import { ProductInvoiceItem } from '@pengode/product-invoice-item/product-invoice-item'
+import { ProductController } from '@pengode/product/product.controller'
+import { ProductService } from '@pengode/product/product.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, ProductCategory])],
+  imports: [
+    TypeOrmModule.forFeature([Product, ProductCategory, ProductInvoiceItem]),
+  ],
   controllers: [ProductController],
   providers: [ProductService],
 })
