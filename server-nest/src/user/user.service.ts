a6687636670206870e2ae532c31387dcc6053360
Tubagus Saifulloh <dutapanggulu@gmail.com>
2024-01-27 14:49:00 +0700

    add method findAll

--- server-nest/src/user/user.service.ts
+++ server-nest/src/user/user.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
-import { Repository } from 'typeorm'
+import { LessThan, MoreThan, Raw, Repository } from 'typeorm'
 
 import { User } from '@pengode/user/user'
 import { UserResponse } from '@pengode/user/user.dto'
+import { FindAllRequest } from '@pengode/article-category/article-category.dto'
+import { PageResponse } from '@pengode/common/dtos'
 
 @Injectable()
 export class UserService {
@@ -12,9 +14,28 @@ export class UserService {
     private readonly userRepository: Repository<User>,
   ) {}
 
-  async findAll(): Promise<UserResponse[]> {
-    const users = await this.userRepository.find({ relations: { roles: true } })
-    return users.map(UserResponse.create)
+  async findAll(req: FindAllRequest): Promise<PageResponse<UserResponse>> {
+    const users = await this.userRepository.find({
+      where: {
+        id: req.previousCursor
+          ? MoreThan(req.previousCursor)
+          : LessThan(req.nextCursor),
+
+        name: req.search
+          ? Raw((alias) => `LOWER(${alias}) LIKE '%${req.search}%'`)
+          : undefined,
+      },
+      take: req.size,
+      order: {
+        id: 'DESC',
+      },
+    })
+
+    return {
+      items: users.map(UserResponse.create),
+      previousCursor: users[0]?.id || 0,
+      nextCursor: users[users.length - 1]?.id || 0,
+    }
   }
 
   async findById(userId: number): Promise<UserResponse> {
