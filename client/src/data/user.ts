0c0378cb060e01970e989ecea4d59aeae7cc10eb
Tubagus Saifulloh <dutapanggulu@gmail.com>
2024-01-29 04:24:05 +0700

    create function to fetch list of users

--- client/src/data/user.ts
+++ client/src/data/user.ts
@@ -2,6 +2,7 @@
 
 import { get, withAuth } from '@pengode/common/rest-client'
 import { env } from '@pengode/common/utils'
+import { Cursor, Page } from '@pengode/data/types'
 
 export interface User {
   id: number
@@ -17,4 +18,20 @@ export async function getAuthUser() {
   return await withAuth(get)<User>({
     url: `${env('PENGODE_API_BASE_URL')}/user`,
   })
+}
+
+export async function getUsers(req?: {
+  cursor?: Cursor
+  size?: number
+  search?: string
+}) {
+  const url = new URL(`${env('PENGODE_API_BASE_URL')}/users`)
+  if (req?.cursor?.nextCursor)
+    url.searchParams.append('nextCursor', `${req.cursor?.nextCursor}`)
+  if (req?.cursor?.previousCursor)
+    url.searchParams.append('previousCursor', `${req.cursor?.previousCursor}`)
+  if (req?.size) url.searchParams.append('size', `${req.size}`)
+  if (req?.search) url.searchParams.append('search', req.search)
+
+  return await withAuth(get)<Page<User>>({ url })
 }
